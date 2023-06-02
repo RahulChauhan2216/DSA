09d94d596c2992b68e48976eeb794986d5906ef3
Rahul <royalchauhan300@gmail.com>
2023-06-02 23:22:21 +0530

    jewels and stones

---
+++ Jewels and Stones/java.js
@@ -0,0 +1,15 @@
+let jewels = "Aaa", stones = "aAAbbbb"
+
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0;
+
+    for (let i = 0; i < jewels.length; i++) {
+        for (let j = 0; j < stones.length; j++) {
+            if (jewels[i] === stones[j]) {
+                count++
+            }
+        }
+    }
+    return count;   
+};
+console.log(numJewelsInStones(jewels, stones));
