ec5d9ed6d9ae13d67bf7f53deb9f52b7a9879bc9
Rahul <royalchauhan300@gmail.com>
2023-06-07 21:05:19 +0530

    Check If Two String Arrays are Equivalent

---
+++ Check If Two String Arrays are Equivalent/java.js
@@ -0,0 +1,41 @@
+let word1 = ["ab", "c"], word2 = ["a", "bc"];
+
+
+let arrayStringsAreEqual = function (word1, word2) {
+
+    let str1 = "";
+    let str2 = "";
+
+    for (let i = 0; i < word1.length; i++) {
+        str1 += word1[i];
+    }
+
+    for (let j = 0; j < word2.length; j++) {
+        str2 += word2[j];
+    }
+
+    if (str1 == str2) {
+        return true
+    }
+    else{
+        return false
+    }
+
+};
+
+console.log(arrayStringsAreEqual(word1, word2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
