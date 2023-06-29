403cea562a9ce9ee771f8b34e4b72e3cc9f7d7e0
Rahul <royalchauhan300@gmail.com>
2023-06-29 22:23:18 +0530

    Destination City

---
+++ Destination City/java.js
@@ -0,0 +1,17 @@
+let paths = [["qMTSlfgZlC", "ePvzZaqLXj"], ["xKhZXfuBeC", "TtnllZpKKg"], ["ePvzZaqLXj", "sxrvXFcqgG"], ["sxrvXFcqgG", "xKhZXfuBeC"], ["TtnllZpKKg", "OAxMijOZgW"]]
+
+let destCity = function (paths) {
+
+    let x = paths[0][1];
+
+    for (let i = 1; i < paths.length; i++) {
+        for (let j = 1; j < paths.length; j++) {
+            if (x == paths[j][0]) {
+                x = paths[j][1];
+            }
+        }
+    }
+    return x;
+};
+
+console.log(destCity(paths));
