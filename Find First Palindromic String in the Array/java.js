e1a1ac02610a33edc8891790533b89751e80f5c0
Rahul <royalchauhan300@gmail.com>
2023-07-04 22:28:26 +0530

    Find First Palindromic String in the Array

---
+++ Find First Palindromic String in the Array/java.js
@@ -0,0 +1,29 @@
+let words = ["def","ghi"];
+
+let firstPalindrome = function (words) {
+
+    let str1 = "";
+    let str2 = "";
+    let count = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length / 2; j++) {
+            str1 += words[i][j];
+            str2 += words[i][words[i].length - j - 1];
+        }
+        if (str1 == str2) {
+            count++;
+            return words[i];
+        }
+        else {
+            str1 = "";
+            str2 = "";
+        }
+    }
+
+    if (count == 0) {
+        return ""
+    }
+};
+
+console.log(firstPalindrome(words));
