4545fc668e3ece2148cb593696155a870639d142
Rahul <royalchauhan300@gmail.com>
2023-08-14 21:22:48 +0530

    Shuffle the Array

---
+++ Shuffle the Array/java.js
@@ -0,0 +1,12 @@
+let nums = [2, 5, 1, 3, 4, 7],
+  n = 3;
+
+let shuffle = function (nums, n) {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(nums[i], nums[i + n]);
+  }
+  return arr;
+};
+
+console.log(shuffle);
