585f0badf50ec0f97c404e80fcfb7565fced1bd5
Rahul <royalchauhan300@gmail.com>
2023-08-29 14:51:12 +0530

    correction in number of triplets

--- Number of Arithmetic Triplets/java.js
+++ Number of Arithmetic Triplets/java.js
@@ -3,15 +3,18 @@ let nums = [0, 1, 4, 6, 7, 10],
 
 let arithmeticTriplets = function (nums, diff) {
   let count = 0;
-
-  for (let i = nums.length - 1; i >= 0; i--) {
-    for (let j = i - 2; j > i - 3; j--) {
-      if (nums[i] - nums[j] == diff) {
-        count++;
+  if (nums.length < 3) {
+    return count;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[k] - nums[j] == diff && nums[j] - nums[i] == diff) {
+          count++;
+        }
       }
     }
   }
-
   return count;
 };
 
