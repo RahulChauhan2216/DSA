96b87a9f582c111cc6c006ca02c8980cee1d9585
Rahul <royalchauhan300@gmail.com>
2023-11-08 22:00:56 +0530

    Minimum String Length After Removing Substrings

---
+++ Minimum String Length After Removing Substrings/java.js
@@ -0,0 +1,15 @@
+let s = "ABFCACDB";
+
+var minLength = function (s) {
+  while (s.includes("AB") || s.includes("CD")) {
+    if (s.includes("AB")) {
+      s = s.replace("AB", "");
+    }
+    if (s.includes("CD")) {
+      s = s.replace("CD", "");
+    }
+  }
+  return s.length;
+};
+
+console.log(minLength(s));
